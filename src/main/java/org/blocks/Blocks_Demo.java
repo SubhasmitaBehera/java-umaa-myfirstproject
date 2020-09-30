087d7311c69548743bc953958a8faf5529279493
SubhasmitaBehera <beherasubhasmita1999@gmail.com>
2020-09-30 12:03:48 +0530

    Blocks | Identifiers

--- src/main/java/org/blocks/Blocks_Demo.java
+++ src/main/java/org/blocks/Blocks_Demo.java
@@ -25,10 +25,11 @@ public class Blocks_Demo {
             args[0] = "Subhasmita";
             args[1] = "Sangita";
             args[2] = "Ritu";
+            args[3] = "Kathanee";
         }
         System.out.println("Array 1st index value "+args[0]);
         System.out.println("Array 2nd index value "+args[1]);
-        System.out.println("Array 3nd index value "+args[2]);
+        System.out.println("Array 33d index value "+args[2]);
         System.out.println("Array 5th index value "+args[5]);
     }
 }
--- src/main/java/org/blocks/Static_NonStaticBlock.java
+++ src/main/java/org/blocks/Static_NonStaticBlock.java
@@ -4,18 +4,19 @@ public class Static_NonStaticBlock {
     static int a;
     static{
         a = 10;
-        System.out.println("Static Block...");
+        System.out.println("This is Static Block...");
     }
 
     {
-        System.out.println("Non Static Block...");
+        System.out.println("This is Non Static Block...");
         int result = a+a;
         System.out.println("Result = " +result);
     }
 
     public static void main(String[] args) {
         new Static_NonStaticBlock();
-        System.out.println("Normal statement in Main method...");
+        System.out.println("This is normal statement in Main method...");
+        a = 20;
         new Static_NonStaticBlock();
     }
 }
