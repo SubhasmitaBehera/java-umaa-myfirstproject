9d99d449bc528fd19f5fd4f59656f0398eb97bf7
SubhasmitaBehera <beherasubhasmita1999@gmail.com>
2020-10-11 00:00:20 +0530

    Objects

--- src/main/java/org/objects/AdmissionForm.java
+++ src/main/java/org/objects/AdmissionForm.java
@@ -1,7 +1,37 @@
 package org.objects;
 
-public class AdmissionForm {
-    public static void main(String[] args) {
 
+public class AdmissionForm {
+
+    public String name;
+    public Integer gradMarksInPercentage;
+    public Integer yearOfGraduation;
+    public Integer ojeeRank;
+
+    // no system.out.println statements
+    public Integer getScholarshipAmount()
+    {
+        if(ojeeRank >=1 && ojeeRank <= 100)
+        {
+            return 70;
+        }
+
+        if(ojeeRank >=101 && ojeeRank <= 200)
+        {
+            return 50;
+        }
+
+        if(ojeeRank >=201 && ojeeRank <= 300)
+        {
+            return 30;
+        }
+
+        if(ojeeRank >=301 )
+        {
+            return 0;
+        }
+
+        return 0;
     }
+
 }
